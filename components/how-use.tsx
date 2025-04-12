cabeb9c1daa2df20c56f0608bf338cdbc5284a52
Dimitri-Igoshev <d.o.igoshev@gmail.com>
2025-04-12 18:48:55 +0300

    Update how-use.tsx

--- components/how-use.tsx
+++ components/how-use.tsx
@@ -5,7 +5,7 @@ import { Download } from "./download";
 export const HowUseSection: FunctionComponent = ({}) => {
   return (
     <div className="container mx-auto flex flex-col bg-gray p-[100px] pt-[80px] rounded-2xl mt-[50px]">
-      <h2 className="text-[40px] font-semibold text-center">
+      <h2 className="text-[32px] xl:text-[40px] font-semibold text-center">
         Сохранение захоронений в{" "}
         <span className="text-primary">несколько кликов</span>
       </h2>
@@ -15,21 +15,20 @@ export const HowUseSection: FunctionComponent = ({}) => {
           alt=""
           radius="none"
           // height={843}
-          width={"100%"}
           src="/step1.png"
-          className="group-hover:translate-x-5 transition-all"
+          className="group-hover:translate-x-5 transition-all w-4/5 xl:w-full"
         />
 
-        <div className="flex flex-col bg-primary p-[60px] rounded-[50px] -ml-[140px] pl-[140px] text-white w-2/3">
+        <div className="flex flex-col bg-primary p-[40px] xl:p-[60px] rounded-[50px] -ml-[190px] xl:-ml-[140px] pl-[80px] xl:pl-[140px] text-white w-2/3">
           <div className="flex gap-7">
-            <div className="rounded-full bg-white min-w-[60px] h-[60px] flex items-center justify-center text-primary text-[40px] font-semibold">
+            <div className="rounded-full bg-white min-w-[40px] h-[40px] xl:min-w-[60px] xl:h-[60px] flex items-center justify-center text-primary text-[24px] xl:text-[40px] font-semibold">
               1
             </div>
             <div>
-              <h4 className="text-[40px] font-semibold -mt-[10px]">
+              <h4 className="text-[24px] xl:text-[40px] font-semibold -mt-[6px] xl:-mt-[10px]">
                 Перейди на вкладку Захоронения
               </h4>
-              <p className="text-[24px] mt-[30px]">
+              <p className="text-[16px] xl:text-[24px] mt-5 xl:mt-[30px]">
                 Тут информация список всех сохраненных вами захоронений. Вы
                 быстро сможете найти захоронение и найти локацию.
               </p>
@@ -39,16 +38,16 @@ export const HowUseSection: FunctionComponent = ({}) => {
       </div>
 
       <div className="flex justify-center items-center group -mt-[100px]">
-        <div className="flex flex-col bg-primary p-[60px] rounded-[50px] -mr-[140px] pr-[140px] text-white w-2/3">
+        <div className="flex flex-col bg-primary p-[40px] xl:p-[60px] rounded-[50px] -mr-[190px] xl:-mr-[140px] pr-[150px] xl:pr-[140px] text-white w-2/3">
           <div className="flex gap-7">
-            <div className="rounded-full bg-white min-w-[60px] h-[60px] flex items-center justify-center text-primary text-[40px] font-semibold">
+            <div className="rounded-full bg-white min-w-[40px] h-[40px] xl:min-w-[60px] xl:h-[60px] flex items-center justify-center text-primary text-[24px] xl:text-[40px] font-semibold">
               2
             </div>
             <div>
-              <h4 className="text-[40px] font-semibold -mt-[10px]">
+              <h4 className="text-[24px] xl:text-[40px] font-semibold -mt-[6px] xl:-mt-[10px]">
                 Заполни данные
               </h4>
-              <p className="text-[24px] mt-[30px]">
+              <p className="text-[16px] xl:text-[24px] mt-5 xl:mt-[30px]">
                 Мы навсегда сохраним для вас всю информацию о захоронении вашего
                 родственника. Включая фотографии и точную геолокацию для
                 удобного поиска
@@ -61,9 +60,9 @@ export const HowUseSection: FunctionComponent = ({}) => {
           alt=""
           radius="none"
           // height={843}
-          width={"100%"}
+          // width={"100%"}
           src="/step2.png"
-          className="group-hover:-translate-x-5 transition-all"
+          className="group-hover:-translate-x-5 transition-all w-4/5 xl:w-full"
         />
       </div>
 
@@ -72,21 +71,21 @@ export const HowUseSection: FunctionComponent = ({}) => {
           alt=""
           radius="none"
           // height={843}
-          width={"100%"}
+          // width={"100%"}
           src="/step3.png"
-          className="group-hover:translate-x-5 transition-all"
+          className="group-hover:translate-x-5 transition-all w-4/5 xl:w-full"
         />
 
-        <div className="flex flex-col bg-primary p-[60px] rounded-[50px] -ml-[140px] pl-[140px] text-white w-2/3">
+        <div className="flex flex-col bg-primary p-[40px] xl:p-[60px] rounded-[50px] -ml-[210px] xl:-ml-[140px] pl-[80px] xl:pl-[140px] text-white w-2/3">
           <div className="flex gap-7">
-            <div className="rounded-full bg-white min-w-[60px] h-[60px] flex items-center justify-center text-primary text-[40px] font-semibold">
+            <div className="rounded-full bg-white min-w-[40px] h-[40px] xl:min-w-[60px] xl:h-[60px] flex items-center justify-center text-primary text-[24px] xl:text-[40px] font-semibold">
               3
             </div>
             <div>
-              <h4 className="text-[40px] font-semibold -mt-[10px]">
+              <h4 className="text-[24px] xl:text-[40px] font-semibold -mt-[6px] xl:-mt-[10px]">
                 Оплати размещение
               </h4>
-              <p className="text-[24px] mt-[30px]">
+              <p className="text-[16px] xl:text-[24px] mt-5 xl:mt-[30px]">
                 Благодаря тому что сервис платный, мы сохраним информацию о
                 захоронении ваших родственников <b>навсегда</b>
               </p>
