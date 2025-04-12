8432a3e68985fc3b017e2f54083a67228e134e48
Dimitri-Igoshev <d.o.igoshev@gmail.com>
2025-04-12 20:10:42 +0300

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -71,7 +71,7 @@ export const Navbar = () => {
       </div>
 
       {mobileMenu ? (
-        <div className="bg-white min-h-screen w-full p-8 pt-[100px] flex flex-col gap-8 text-lg">
+        <div className="bg-white min-h-screen h-full w-full p-8 pt-[100px] flex flex-col gap-8 text-lg">
           <div className="cursor-pointer hover:text-primary">Поиск</div>
           <div className="cursor-pointer hover:text-primary">
             Мемориалы памяти
