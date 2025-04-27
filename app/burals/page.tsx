c112e6d26474fa22f8530bd68441bb9125ab2ea1
Dimitri Igoshev <61008146+Dimitri-Igoshev@users.noreply.github.com>
2025-04-27 13:17:54 +0300

    Create page.tsx

---
+++ app/burals/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useGetTombsMutation } from "@/redux/services/buralApi";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+const BuralsPage = () => {
+	const searchParams = useSearchParams();
+
+	const [search, setSearch] = useState<any>({
+		query: searchParams.get("query") || "",
+		birthday: searchParams.get("birthday") || "",
+		deathday: searchParams.get("maxage") || "",
+		limit: 20,
+		offset: 0,
+	});
+
+	const [burals, setBurals] = useState<any[]>([]);
+
+	const [getTombs] = useGetTombsMutation();
+
+	const getBurals = async () => {
+		await getTombs(search).unwrap().then((res: any) => setBurals(res?.tombs || [])).catch((err) => console.log(err));
+	};
+
+	useEffect(() => {
+		setSearch({
+			...search,
+			query: searchParams.get("query") || "",
+			birthday: searchParams.get("birthday") || "",
+			deathday: searchParams.get("maxage") || "",
+		});
+
+    getBurals();
+
+    console.log(burals)
+	}, [searchParams]);
+
+	return <div className="min-h-screen pt-[100px]">BuralsPage</div>;
+};
+
+export default BuralsPage;
